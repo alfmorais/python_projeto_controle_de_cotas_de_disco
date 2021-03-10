cbe714069cf2fd5f88116d2d68c43b1ff7c15a92
Alfredo de Morais <alfredneto@hotmail.com>
2021-03-10 18:51:08 -0300

    gerador de dados ok

--- versao_2/funcoes.py
+++ versao_2/funcoes.py
@@ -5,3 +5,47 @@ Desenvolvedor: Alfredo de Morais
 Objetivo: Elaborar todas as funções que são usado no arquivo
 
 '''
+
+
+def leitura_arquivo_usuario():
+    pass
+
+
+def leitura_arquivo_programa():
+    pass
+
+
+def limpando_dados():
+    pass
+
+
+def eliminando_espaços():
+    pass
+
+
+def lista_bits():
+    pass
+
+
+def lista_usuarios():
+    pass
+
+
+def conversao_bytes():
+    pass
+
+
+def calculo_medio():
+    pass
+
+
+def calculo_porcentagem():
+    pass
+
+
+def dataframe():
+    pass
+
+
+def relatorio():
+    pass
--- versao_2/gerador_de_dados.py
+++ versao_2/gerador_de_dados.py
@@ -1,16 +1,15 @@
 '''
 08/03/2021
-
 Desenvolvedor: Alfredo de Morais
 Objetivo: Elaborar todas as funções que são usado no arquivo
-
 '''
+
 import pandas as pd
-from random import random
+import random as rand
 
 
 # Caminho do arquivo nomes.txt -> OBS: Sempre verificar o path
-caminho = 'C:/Users/Alfredo/github/python_projeto_controle_de_cotas_de_disco/python_projeto_controle_de_cotas_de_disco-1/versao_2/nomes.txt'
+caminho = 'C:/Users/Alfredo/github/python_projeto_controle_de_cotas_de_disco-1/versao_2/nomes.txt'
 
 # Lista vazia para nomes
 nomes = []
@@ -23,37 +22,38 @@ with open(caminho, 'r') as objeto_arquivo:
         nomes.append(linha)
 
 # Lista de números para simular a quantidades de dados
-dados = list(range(999999999))
-
-# Função que retorna a quantidade de funcionários e dados referentes ao arquivo teste
+dados = list(range(99999))
 
 
 def dados_teste(nomes, dados, quantidade):
     '''
-    Exemplo de uso da função:
+    Função que retorna a quantidade de funcionários e dados referentes ao 
+    arquivo teste, Exemplo de uso da função:
 
     codigo:
+
     arquivo = dados_teste(lista, lista, int)
 
-    o retorno da função será um arquivo txt com dados selecionados de maneira randomica. 
+    o retorno da função será um arquivo txt com dados selecionados de maneira 
+    randomica. 
     '''
 
     # Função random.choices para o nome e dados
-    lista_nome = random.choices(nomes, k=quantidade)
-    lista_dado = random.choices(dados, k=quantidade)
+    lista_nome = rand.sample(nomes, quantidade)
+    lista_dado = rand.sample(dados, quantidade)
 
     # Dataframe com os dados de teste
     arquivo_dados = pd.DataFrame({
-        lista_nome,
-        lista_dado,
+        'Nome': lista_nome,
+        'Dado': lista_dado,
     })
 
     # Elaboração do arquivo txt para teste
-    with open('dados_de_texte.txt', 'w') as arquivo_dados_de_teste:
+    with open('dados_de_texto.txt', 'w') as arquivo_dados_de_teste:
         arquivo_dados_de_teste.write(arquivo_dados.to_string())
 
     # Retorno do arquivo dados de teste
     return arquivo_dados_de_teste
 
 
-arquivo = dados_teste(nomes, dados, 6)
+arquivo = dados_teste(nomes, dados, 99)
