bfbee74ab5bae8c0f4ed92fbda46ec515209cf16
Alfredo de Morais <alfredneto@hotmail.com>
2021-03-09 21:42:35 -0300

    lendo arquivo

--- versao_2/gerador_de_dados.py
+++ versao_2/gerador_de_dados.py
@@ -5,19 +5,16 @@ Desenvolvedor: Alfredo de Morais
 Objetivo: Elaborar todas as funções que são usado no arquivo
 
 '''
-
+import re
 import pandas as pd
 
 caminho = 'C:/Users/Alfredo/AppData/Local/Programs/Python/Python39/python_projeto_controle_de_cotas_de_disco/versao_2/nomes.txt'
 
-lista_nomes = []
+nomes = []
 
-with open(caminho, 'r') as arquivo:
-    print(arquivo.read())
-    '''
-    for linha in arquivo_aberto:
-        linha = linha.rstrip()
-        lista_nomes.append(linha)
+with open(caminho, 'r') as objeto_arquivo:
+    arquivo = objeto_arquivo.read()
+    for linha in arquivo:
+        nomes.append(linha)
 
-print(lista_nomes)
-'''
+print(nomes)
